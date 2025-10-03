d9c9815bce87a1912d7cb300559017d10f9969c9
Johan Mesa <Johanotr@gmail.com>
2025-10-03 09:49:22 -0400

    style: add background image

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,19 @@ const App = () => {
   
   return (
     <div className={styles.mainMenu}>
-      <h1 className={styles.title}>Where Are They?</h1>
-      <div className={styles.actions}>
-        <button className={styles.btn}>Start Game</button>
-        <button className={styles.btn}>LeaderBoard</button>
+      <div className={styles.blurredOverlay}>
+        <h1 className={styles.title}>Where Are They?</h1>
+        <div className={styles.actions}>
+          <button className={styles.btn}>Start Game</button>
+          <button className={styles.btn}>LeaderBoard</button>
+        </div>
+        <ol className={styles.rulesList}>
+          <li>Start the game to begin the timer.</li>
+          <li>Search the picture for 3 hidden characters.</li>
+          <li>Click a character and confirm who you found.</li>
+          <li>Find all 3 as fast as you can!</li>
+        </ol>
       </div>
-      <ol className={styles.rulesList}>
-        <li>Start the game to begin the timer.</li>
-        <li>Search the picture for 3 hidden characters.</li>
-        <li>Click a character and confirm who you found.</li>
-        <li>Find all 3 as fast as you can!</li>
-      </ol>
     </div>
   )
 }
