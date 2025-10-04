6a22f172cf430fcae21d343a65819391cb0418f0
Johan Mesa <Johanotr@gmail.com>
2025-10-04 12:19:46 -0400

    feat: configure react router

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./app.module.css";
-
-const App = () => {
-  return <></>;
-};
-
-export default App;
---
+++ src/routes.jsx
@@ -0,0 +1,10 @@
+import Welcome from "./pages/Welcome";
+
+const routes = [
+    {
+        path: "/",
+        element: <Welcome />
+    }
+]
+
+export default routes;
