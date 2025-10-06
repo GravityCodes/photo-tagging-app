382f94911b0e43f7bd61c93aa6496057a54311d8
Johan Mesa <Johanotr@gmail.com>
2025-10-06 14:42:31 -0400

    feat: add quit button

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,10 +1,11 @@
 import styles from "./game.module.css";
-import { useEffect, useState } from "react";
-
+import { useEffect, useState, useRef } from "react";
+import { Link } from "react-router";
 const Game = () => {
     
     const [second, setSeconds] = useState(0);
     const [minute, setMinute] = useState(0);
+    const dialog = useRef(null);
 
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -20,6 +21,10 @@ const Game = () => {
         return () => clearInterval(intervalId);
     });
 
+    const showModal = () => {
+        dialog.current.showModal();
+    }
+
     return (
         <div className={styles.gameContainer}>
             <div className={styles.topBar}>
@@ -27,11 +32,23 @@ const Game = () => {
                     <img src="./clock.svg" alt="timer" />
                     <div className={styles.timer}>{second < 10 ? `${minute}:0${second}` : `${minute}:${second}` }</div>
                 </div>
-                <button>Give up</button>
+                <div>
+                    <img src="./characters/character1.png" alt="character 1" />
+                    <img src="./characters/character2.png" alt="character 2" />
+                    <img src="./characters/character3.png" alt="character 3" />
+                </div>
+                <button className={styles.quitBtn} onClick={showModal}>Give up</button>
             </div>
             <div className={styles.gameMap}>
                 <img src="./gameImage.jpg" alt="game image" />
             </div>
+            <dialog ref={dialog}>
+                <p>Are you sure you want to give up?</p>
+                <form method="dialog" className={styles.dialogForm}>
+                    <button className={styles.btn}>No</button>
+                    <Link to="/"><button className={styles.btn}>Yes</button></Link>
+                </form>
+            </dialog>
         </div>
     )
 }
