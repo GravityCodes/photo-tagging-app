0c04c59a01502ad56e2cc01eb6dcd11ac3ea43cf
Johan Mesa <Johanotr@gmail.com>
2025-10-06 10:00:04 -0400

    feat: add assets to game page

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,8 +1,21 @@
+import styles from "./game.module.css";
+import { useState } from "react";
+
 const Game = () => {
+    
     return (
-        <>
-            This is the game
-        </>
+        <div className={styles.gameContainer}>
+            <div className={styles.topBar}>
+                <div className={styles.timerContainer}>
+                    <img src="./clock.svg" alt="timer" />
+                    <div className={styles.timer}></div>
+                </div>
+                <button>Give up</button>
+            </div>
+            <div className={styles.gameMap}>
+                <img src="./gameImage.jpg" alt="game image" />
+            </div>
+        </div>
     )
 }
 
