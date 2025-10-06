8ba63263e1112533aa4a9144504d07046cbc2c92
Johan Mesa <Johanotr@gmail.com>
2025-10-06 11:53:34 -0400

    feat: add timer

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,14 +1,31 @@
 import styles from "./game.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Game = () => {
     
+    const [second, setSeconds] = useState(0);
+    const [minute, setMinute] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setSeconds(s => s + 1);
+
+        }, 1000);
+
+        if(second == 60){
+            setMinute(m => m + 1);
+            setSeconds(0);
+        }
+
+        return () => clearInterval(intervalId);
+    });
+
     return (
         <div className={styles.gameContainer}>
             <div className={styles.topBar}>
                 <div className={styles.timerContainer}>
                     <img src="./clock.svg" alt="timer" />
-                    <div className={styles.timer}></div>
+                    <div className={styles.timer}>{second < 10 ? `${minute}:0${second}` : `${minute}:${second}` }</div>
                 </div>
                 <button>Give up</button>
             </div>
