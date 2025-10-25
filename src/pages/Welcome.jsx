3930fef7751eddd02c3bdd98e11690456ebb6ce9
Johan Mesa <Johanotr@gmail.com>
2025-10-24 23:50:11 -0400

    style: refactor welcome page

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router";
 const Welcome = () => {
   return (
     <div className={styles.mainMenu}>
-      <div className={styles.blurredOverlay}>
         <h1 className={styles.title}>Where Are They?</h1>
         <div className={styles.actions}>
           <Link to="/game">
@@ -19,12 +18,12 @@ const Welcome = () => {
           </Link>
         </div>
         <ol className={styles.rulesList}>
+          <p>How to Play</p>
           <li>Start the game to begin the timer.</li>
           <li>Search the picture for 3 hidden characters.</li>
           <li>Click a character and confirm who you found.</li>
           <li>Find all 3 as fast as you can!</li>
         </ol>
-      </div>
     </div>
   );
 };
